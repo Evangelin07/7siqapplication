047d55aedfb82619bb2fd2227ebdf1e63af47e1e
Evangelin07 <evangelindurai27@gmail.com>
2026-01-11 06:25:38 +0530

    Update backend + frontend for Render deploy

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Backend is running successfully 🚀");
+  res.sendFile(path.join(__dirname,'public/index.html'));
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -214,5 +214,5 @@ if (formData.photo) {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log('Backend is running successfully on port $ {PORT}');
 });
